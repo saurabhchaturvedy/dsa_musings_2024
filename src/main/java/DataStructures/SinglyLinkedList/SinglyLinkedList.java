4cba4649e09f41042d9db8c0876d502a22f2c972
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2024-08-02 18:10:51 +0530

    dsa musings 2024

---
+++ src/main/java/DataStructures/SinglyLinkedList/SinglyLinkedList.java
@@ -0,0 +1,25 @@
+package DataStructures.SinglyLinkedList;
+
+public class SinglyLinkedList {
+
+
+    private ListNode head;
+
+    private static class ListNode
+    {
+
+        int data;
+        ListNode next;
+
+        ListNode(int data)
+        {
+
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
