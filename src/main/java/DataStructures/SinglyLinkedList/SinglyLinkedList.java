b8038685e772aaa9ed95fe18163e68a9d5a42cd1
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2024-08-02 18:17:28 +0530

    dsa musings 2024

--- src/main/java/DataStructures/SinglyLinkedList/SinglyLinkedList.java
+++ src/main/java/DataStructures/SinglyLinkedList/SinglyLinkedList.java
@@ -21,5 +21,12 @@ public class SinglyLinkedList {
 
     public static void main(String[] args) {
 
+
+        SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
+
+        singlyLinkedList.head = new ListNode(10);
+        ListNode second = new ListNode(20);
+        ListNode third = new ListNode(30);
+        ListNode fourth = new ListNode(40);
     }
 }
