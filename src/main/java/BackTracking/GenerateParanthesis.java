e9aa4f6764a7090c9e59a019e4f1047e1c0069b4
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2024-08-04 00:42:59 +0530

    dsa musings 2024

---
+++ src/main/java/BackTracking/GenerateParanthesis.java
@@ -0,0 +1,53 @@
+package BackTracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateParanthesis {
+
+
+    public List<String> response = new ArrayList<>();
+
+
+    public List<String> generateParanthesis(int n) {
+
+        dfs(new StringBuilder(), 0, n);
+        return response;
+    }
+
+
+    public void dfs(StringBuilder sb, int close, int n) {
+
+
+        if (close == 0 && n == 0) {
+            response.add(sb.toString());
+            return;
+        }
+
+
+        if (n > 0) {
+            sb.append('(');
+            dfs(sb, close + 1, n - 1);
+            sb.setLength(sb.length() - 1);
+        }
+
+
+        if (close > 0) {
+
+            sb.append(')');
+            dfs(sb, close - 1, n);
+            sb.setLength(sb.length() - 1);
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+
+        GenerateParanthesis generateParanthesis = new GenerateParanthesis();
+
+        List<String> strings = generateParanthesis.generateParanthesis(2);
+
+        System.out.println(strings);
+    }
+}
