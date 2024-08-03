31ae0e57c7b2b627e704f8de22ddb203f43fd204
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2024-08-03 16:53:10 +0530

    dsa musings 2024

--- src/main/java/DaveFreinBerg/Recursion/Recursion.java
+++ src/main/java/DaveFreinBerg/Recursion/Recursion.java
@@ -4,17 +4,34 @@ public class Recursion {
 
 
     public static void mystery(int count) {
-        if(count>0) {
+        if (count > 0) {
             System.out.println("Hello");
-            mystery(count-1);
+            mystery(count - 1);
+
         }
 
 
     }
 
 
+    public static int factorial(int n) {
+
+
+        if (n == 0) {
+            return 1;
+        } else {
+
+            int result = factorial(n - 1);
+            return result * n;
+        }
+    }
+
+
     public static void main(String[] args) {
 
         mystery(5);
+
+        System.out.println(factorial(9));
+
     }
 }
