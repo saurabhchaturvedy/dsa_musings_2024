f6320986f0d05d62da64e4e83fde85d23656b4bc
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2024-08-07 22:25:03 +0530

    dsa musings 2024

---
+++ src/main/java/BackTracking/RecursionTest.java
@@ -0,0 +1,35 @@
+package BackTracking;
+
+public class RecursionTest {
+
+
+    public void print(int[] arr, int index) {
+
+        if (index > -1) {
+            System.out.println(arr[index]);
+            print(arr, index - 1);
+        }
+    }
+
+
+    public static void printX(int[] arr, int index) {
+
+        if (index > -1) {
+            printX(arr, index - 1);
+            System.out.println(arr[index]);
+
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+
+        RecursionTest recursionTest = new RecursionTest();
+
+        int[] arr = {1, 2, 3, 4, 5};
+        recursionTest.print(arr, arr.length - 1);
+        System.out.println();
+        printX(arr, arr.length-1);
+    }
+}
