92a35684e7b6a55ff5089910a7016b9063b2ea7b
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2024-08-10 18:01:41 +0530

    dsa musings 2024

---
+++ src/main/java/BackTracking/GeneratePermutations.java
@@ -0,0 +1,43 @@
+package BackTracking;
+
+public class GeneratePermutations {
+
+
+    public static void dfs(String str, int left, int right) {
+
+
+        if (left == right) {
+            System.out.println(str);
+        }
+
+
+        for (int i = left; i <= right; i++) {
+
+            str = swap(str, left, i);
+            dfs(str, left + 1, right);
+            str = swap(str, left, i);
+        }
+    }
+
+
+    public static String swap(String str, int i, int j) {
+
+        char[] ch = str.toCharArray();
+
+        char temp = ch[i];
+        ch[i] = ch[j];
+        ch[j] = temp;
+
+        return new String(ch);
+    }
+
+
+    public static void main(String[] args) {
+
+
+        String str = "ABC";
+
+        dfs(str, 0, str.length() - 1);
+
+    }
+}
