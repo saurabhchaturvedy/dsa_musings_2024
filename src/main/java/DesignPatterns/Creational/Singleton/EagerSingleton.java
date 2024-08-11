a36ebfd3082fcf7ac85b95b5cf6c89fb55721d3f
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2024-08-12 00:24:53 +0530

    dsa musings 2024

--- src/main/java/DesignPatterns/Creational/Singleton/EagerSingleton.java
+++ src/main/java/DesignPatterns/Creational/Singleton/EagerSingleton.java
@@ -1,11 +1,14 @@
 package DesignPatterns.Creational.Singleton;
 
-public class EagerSingleton {
-	private static volatile EagerSingleton instance = new EagerSingleton();
-	// private constructor
-	private EagerSingleton() {
-	}
-	public static EagerSingleton getInstance() {
-	return instance;
-	}
+class EagerSingleton {
+    // The single instance, created immediately
+    private static final EagerSingleton instance = new EagerSingleton();
+    
+    // Private constructor to prevent instantiation
+    private EagerSingleton() {}
+    
+    // Public method to get the instance
+    public static EagerSingleton getInstance() {
+        return instance;
+    }
 }
---
+++ src/main/java/DesignPatterns/Creational/Singleton/StaticBlockSingleton.java
@@ -0,0 +1,23 @@
+package DesignPatterns.Creational.Singleton;
+
+class StaticBlockSingleton {
+    // The single instance
+    private static StaticBlockSingleton instance;
+    
+    // Private constructor to prevent instantiation
+    private StaticBlockSingleton() {}
+    
+    // Static block for initialization
+    static {
+        try {
+            instance = new StaticBlockSingleton();
+        } catch (Exception e) {
+            throw new RuntimeException("Exception occurred in creating singleton instance");
+        }
+    }
+    
+    // Public method to get the instance
+    public static StaticBlockSingleton getInstance() {
+        return instance;
+    }
+}
