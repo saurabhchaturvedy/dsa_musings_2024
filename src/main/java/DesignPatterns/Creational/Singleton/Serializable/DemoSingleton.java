cff4eebf3ff485ed9b350d8c02c921f94a6fc9df
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2024-08-12 00:32:04 +0530

    dsa musings 2024

---
+++ src/main/java/DesignPatterns/Creational/Singleton/Serializable/DemoSingleton.java
@@ -0,0 +1,24 @@
+package DesignPatterns.Creational.Singleton.Serializable;
+
+import java.io.Serializable;
+
+public class DemoSingleton implements Serializable {
+	private volatile static DemoSingleton instance = null;
+	public static DemoSingleton getInstance() {
+		if (instance == null) {
+			instance = new DemoSingleton();
+		}
+		return instance;
+	}
+	private int i = 10;
+	public int getI() {
+		return i;
+	}
+	public void setI(int i) {
+		this.i = i;
+	}
+
+	protected Object readResolve() {
+		return instance;
+	}
+}
---
+++ src/main/java/DesignPatterns/Creational/Singleton/Serializable/SerializationTest.java
@@ -0,0 +1,26 @@
+package DesignPatterns.Creational.Singleton.Serializable;
+
+import java.io.*;
+
+public class SerializationTest {
+	static DemoSingleton instanceOne = DemoSingleton.getInstance();
+	public static void main(String[] args) {
+		try {
+			// Serialize to a file
+			ObjectOutput out = new ObjectOutputStream(new FileOutputStream("filename.ser"));
+			out.writeObject(instanceOne);
+			out.close();
+			instanceOne.setI(20);
+			// Serialize to a file
+			ObjectInput in = new ObjectInputStream(new FileInputStream("filename.ser"));
+			DemoSingleton instanceTwo = (DemoSingleton) in.readObject();
+			in.close();
+			System.out.println(instanceOne.getI());
+			System.out.println(instanceTwo.getI());
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+}
