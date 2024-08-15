31c927671943b6637307007f7bb65c920974ba82
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2024-08-15 20:25:19 +0530

    dsa musings 2024

---
+++ src/main/java/CodingSimplified/Array/BubbleSort.java
@@ -0,0 +1,43 @@
+package CodingSimplified.Array;
+
+public class BubbleSort {
+
+
+    public void bubbleSort(int[] arr) {
+
+
+        if (arr.length == 0 || arr.length == 1) {
+            return;
+        }
+
+
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+
+        int[] arr = {12, 5, 3, -1};
+
+        BubbleSort bubbleSort = new BubbleSort();
+
+        bubbleSort.bubbleSort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
