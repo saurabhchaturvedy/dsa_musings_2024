c22868142d37750e7ab4f7753befab6d45aba55d
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2024-08-15 20:44:12 +0530

    dsa musings 2024

---
+++ src/main/java/CodingSimplified/Array/InsertionSort.java
@@ -0,0 +1,45 @@
+package CodingSimplified.Array;
+
+public class InsertionSort {
+
+
+    public void insertionSort(int[] arr) {
+
+        if (arr.length <= 1) {
+            return;
+        }
+
+
+        for (int i = 1; i < arr.length; i++) {
+
+            int key = arr[i];
+
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+
+            arr[j + 1] = key;
+
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+
+        int[] arr = {5, 2, 14, 6, 3};
+
+        InsertionSort insertionSort = new InsertionSort();
+
+        insertionSort.insertionSort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
