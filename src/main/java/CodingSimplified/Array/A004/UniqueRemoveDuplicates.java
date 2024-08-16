1eb53ba25bf5064a6a80ad1fa23707d7a50dc113
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2024-08-16 23:33:03 +0530

    dsa musings 2024

--- src/main/java/CodingSimplified/Array/A004/UniqueRemoveDuplicates.java
+++ src/main/java/CodingSimplified/Array/A004/UniqueRemoveDuplicates.java
@@ -1,2 +1,32 @@
-package CodingSimplified.Array.A004;public class UniqueRemoveDuplicates {
+package CodingSimplified.Array.A004;
+
+public class UniqueRemoveDuplicates {
+
+
+    public static int removeDuplicateUnique(int[] arr) {
+
+        if (arr.length <= 1) return arr.length;
+
+
+        int start = 1;
+
+        for (int i = 1; i < arr.length; i++) {
+
+            if (arr[i] != arr[i - 1]) {
+
+                arr[start++] = arr[i];
+            }
+        }
+
+        return start;
+    }
+
+
+    public static void main(String[] args) {
+
+
+        int arr[] = {1, 4, 4, 4, 5, 6, 7, 7, 8};
+
+        System.out.println(removeDuplicateUnique(arr));
+    }
 }
