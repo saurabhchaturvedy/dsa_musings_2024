43f0d4faea24e4afc7a2a257200a785335acf5a4
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2024-08-18 16:53:15 +0530

    dsa musings 2024

---
+++ src/main/java/CodingSimplified/LinkedList/LL001/DeleteFrontNode.java
@@ -0,0 +1,34 @@
+package CodingSimplified.LinkedList.LL001;
+
+import CodingSimplified.LinkedList.LinkedListUtils;
+import CodingSimplified.LinkedList.ListNode;
+
+import java.util.LinkedList;
+
+public class DeleteFrontNode {
+
+
+    public static ListNode deleteFront(ListNode node) {
+
+        if (node == null) return null;
+
+        return node.next;
+    }
+
+
+    public static void main(String[] args) {
+
+        ListNode root = null;
+
+        root = LinkedListUtils.insert(10, root);
+        root = LinkedListUtils.insert(20, root);
+        root = LinkedListUtils.insert(30, root);
+        root = LinkedListUtils.insert(40, root);
+        root = LinkedListUtils.insert(50, root);
+
+        LinkedListUtils.print(root);
+
+        root = deleteFront(root);
+        LinkedListUtils.print(root);
+    }
+}
