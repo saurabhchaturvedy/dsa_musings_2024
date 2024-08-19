4ebbd2549de260218cacc3cd706ca96e84ad468d
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2024-08-19 17:11:48 +0530

    dsa musings 2024

---
+++ src/main/java/CodingSimplified/LinkedList/LL005/SwapNodePairs.java
@@ -0,0 +1,56 @@
+package CodingSimplified.LinkedList.LL005;
+
+import CodingSimplified.LinkedList.LinkedListUtils;
+import CodingSimplified.LinkedList.ListNode;
+
+public class SwapNodePairs {
+
+    public static ListNode swapPairs(ListNode head) {
+
+
+        if (head == null) {
+            return head;
+        }
+
+
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+
+        ListNode point = dummy;
+
+        while (point.next != null && point.next.next != null) {
+
+
+            ListNode swap1 = point.next;
+            ListNode swap2 = point.next.next;
+
+            swap1.next = swap2.next;
+            swap2.next = swap1;
+
+            point.next = swap2;
+            point = swap1;
+        }
+
+
+        return dummy.next;
+    }
+
+
+    public static void main(String[] args) {
+
+
+        ListNode root = null;
+        root = LinkedListUtils.insert(1, root);
+        root = LinkedListUtils.insert(2, root);
+        root = LinkedListUtils.insert(3, root);
+        root = LinkedListUtils.insert(4, root);
+        root = LinkedListUtils.insert(5, root);
+        root = LinkedListUtils.insert(6, root);
+
+        LinkedListUtils.print(root);
+
+        root = swapPairs(root);
+
+        LinkedListUtils.print(root);
+    }
+}
