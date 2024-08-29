0f33ad351771505960e1ce302a0b2ad9ea009b32
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2024-08-30 03:19:08 +0530

    dsa musings 2024

---
+++ src/main/java/Learnings/WM202409/PalindromeMinimum2Deletion.java
@@ -0,0 +1,65 @@
+package Learnings.WM202409;
+
+public class PalindromeMinimum2Deletion {
+
+
+    public static boolean isPalindrome(char[] ch, int start, int end) {
+
+        if (ch.length == 0) return false;
+
+        while (start < end) {
+
+            if (ch[start] != ch[end]) {
+
+                return true;
+            }
+
+            start++;
+            end--;
+        }
+
+        return false;
+    }
+
+
+    public static boolean isPalindromeMinimum2Deletion(String str) {
+
+
+        char[] ch = str.toCharArray();
+
+        int start = 0;
+        int end = ch.length - 1;
+        int deletions = 0;
+
+        while (start < end) {
+
+            if (ch[start] == ch[end]) {
+
+                start++;
+                end--;
+            } else {
+
+
+                if (isPalindrome(ch, start + 1, end) || isPalindrome(ch, start, end - 1)) {
+                    deletions++;
+                } else {
+
+                    deletions = deletions + 2;
+                }
+            }
+
+            break;
+        }
+
+        return deletions <= 2;
+    }
+
+
+    public static void main(String[] args) {
+
+
+        String input = "abecbea";
+
+        System.out.println(" can string be palindrome after 2 deletions ? " + isPalindromeMinimum2Deletion(input));
+    }
+}
