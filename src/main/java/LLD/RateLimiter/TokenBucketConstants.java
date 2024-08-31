74c858f33c97d7d3947beefab014bcce3220fdf5
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2024-08-31 15:10:23 +0530

    dsa musings 2024

---
+++ src/main/java/LLD/RateLimiter/TokenBucketConstants.java
@@ -0,0 +1,9 @@
+package LLD.RateLimiter;
+
+public class TokenBucketConstants {
+
+
+    public static final int numberofRequests = 10;
+    public static final int windowSizeForRateLimit = 1 * 1000;
+    public static final int maxBucketSize = 10;
+}
