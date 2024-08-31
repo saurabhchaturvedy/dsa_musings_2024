d850234e5618c5f33d7b61a12c0fd06dfe7a46f9
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2024-09-01 02:23:44 +0530

    dsa musings 2024

---
+++ src/main/java/Learnings/WM202409/Intervals/NonOverlappingIntervals.java
@@ -0,0 +1,32 @@
+package Learnings.WM202409.Intervals;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class NonOverlappingIntervals {
+
+
+    public int eraseOverlapIntervals(int[][] intervals) {
+
+
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[1]));
+
+        int end = intervals[0][1];
+
+
+        int overlaps = 0;
+
+
+        for (int i = 1; i < intervals.length; i++) {
+
+            if (intervals[i][0] >= end) {
+                end = intervals[i][1];
+            } else {
+                overlaps++;
+            }
+        }
+
+
+        return overlaps;
+    }
+}
