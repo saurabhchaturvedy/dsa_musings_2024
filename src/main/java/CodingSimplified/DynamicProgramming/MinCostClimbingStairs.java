ee5c7fc344a5bc5871e0fc8368262047f5fbe837
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2024-09-02 18:51:07 +0530

    dsa musings 2024

---
+++ src/main/java/CodingSimplified/DynamicProgramming/MinCostClimbingStairs.java
@@ -0,0 +1,34 @@
+package CodingSimplified.DynamicProgramming;
+
+public class MinCostClimbingStairs {
+
+
+    public static int minCostClimbingStairs(int[] cost) {
+
+        return Math.min(minCostClimbingStairs(cost, 0), minCostClimbingStairs(cost, 1));
+    }
+
+    private static int minCostClimbingStairs(int[] cost, int i) {
+
+
+        if (i >= cost.length) {
+            return 0;
+        }
+
+
+        int move_one = cost[i] + minCostClimbingStairs(cost, i + 1);
+        int move_two = cost[i] + minCostClimbingStairs(cost, i + 2);
+
+        return Math.min(move_one, move_two);
+    }
+
+
+    public static void main(String[] args) {
+
+
+        int[] arr = {10, 15, 20};
+
+
+        System.out.println(" min cost climbing stairs = " + minCostClimbingStairs(arr));
+    }
+}
