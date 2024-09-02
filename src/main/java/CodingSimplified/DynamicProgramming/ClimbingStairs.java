b3cbb63c75d1f5ad1efd798373a1fed889b6f6ce
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2024-09-02 17:21:07 +0530

    dsa musings 2024

--- src/main/java/CodingSimplified/DynamicProgramming/ClimbingStairs.java
+++ src/main/java/CodingSimplified/DynamicProgramming/ClimbingStairs.java
@@ -21,9 +21,30 @@ public class ClimbingStairs {
     }
 
 
+    public static int climbStairsTopDown(int[] dp, int n) {
+
+        if (n == 0) return 1;
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        if (dp[n] == 0) {
+
+            dp[n] = climbStairsTopDown(dp, n - 1) + climbStairsTopDown(dp, n - 2);
+        }
+
+        return dp[n];
+    }
+
+
     public static void main(String[] args) {
 
 
-        System.out.println(" no of ways to climb stairs = "+climbStairsBottomUp(4));
+        System.out.println(" no of ways to climb stairs = " + climbStairsBottomUp(4));
+
+        int n = 3;
+
+        int[] dp = new int[n + 1];
+
+        System.out.println(" no of way to climb stairs - Top down = " + climbStairsTopDown(dp, n));
     }
 }
