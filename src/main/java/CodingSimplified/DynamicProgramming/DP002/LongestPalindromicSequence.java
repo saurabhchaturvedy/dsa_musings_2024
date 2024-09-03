4986805158f051e5b82844f46e7c797cc03e2cd4
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2024-09-03 22:10:13 +0530

    dsa musings 2024

--- src/main/java/CodingSimplified/DynamicProgramming/DP002/LongestPalindromicSequence.java
+++ src/main/java/CodingSimplified/DynamicProgramming/DP002/LongestPalindromicSequence.java
@@ -42,6 +42,60 @@ public class LongestPalindromicSequence {
     }
 
 
+    static int lps(String s, int start, int end, Integer[][] dp) {
+
+        if (start > end) {
+            return 0;
+        }
+
+        if (start == end) {
+            return 1;
+        }
+
+        //  System.out.println(start);
+        //System.out.println(end);
+
+        if (dp[start][end] == null) {
+            if (s.charAt(start) == s.charAt(end)) {
+
+                dp[start][end] = 2 + lps(s, start + 1, end - 1, dp);
+            } else {
+
+                dp[start][end] = Math.max(lps(s, start + 1, end, dp), lps(s, start, end - 1, dp));
+            }
+        }
+        return dp[start][end];
+
+    }
+
+
+    public static int longestPalindromeSubseqBottomUp(String s) {
+
+        int[][] dp = new int[s.length()][s.length()];
+
+        for (int i = 0; i < s.length(); i++) {
+
+            dp[i][i] = 1;
+        }
+
+        for (int i = s.length() - 2; i >= 0; i--) {
+
+            for (int j = i + 1; j < s.length(); j++) {
+
+                if (s.charAt(i) == s.charAt(j)) {
+
+                    dp[i][j] = 2 + dp[i + 1][j - 1];
+                } else {
+
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+
+        }
+        return dp[0][s.length() - 1];
+    }
+
+
     public static void main(String[] args) {
 
 
@@ -49,5 +103,9 @@ public class LongestPalindromicSequence {
 
 
         System.out.println(" longest palindromic substring = " + longestPalindromeSubseq(s));
+
+        System.out.println(" longest palindromic substring = " + lps(s, 0, s.length() - 1, new Integer[s.length()][s.length()]));
+
+        System.out.println("longest palindromic string bottom up = " + longestPalindromeSubseqBottomUp(s));
     }
 }
