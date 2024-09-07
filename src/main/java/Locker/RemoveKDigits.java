6bc0b052e0884f63d1ad2ad1f1bc9e0de3b0d827
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2024-09-07 23:58:34 +0530

    dsa musings 2024

---
+++ src/main/java/Locker/RemoveKDigits.java
@@ -0,0 +1,54 @@
+package Locker;
+
+public class RemoveKDigits {
+
+
+    public String removeKdigits(String num, int k) {
+
+        if (k == 0) {
+            return num;
+        }
+
+        if (k == num.length()) {
+
+            return "";
+        }
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < num.length(); i++) {
+
+            while (!stack.isEmpty() && k > 0 && stack.peek() > num.charAt(i)) {
+
+                stack.pop();
+                k--;
+            }
+
+            stack.push(num.charAt(i));
+
+        }
+
+        for (int i = 0; i < k; i++) {
+
+            stack.pop();
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        while (!stack.isEmpty()) {
+
+            sb.append(stack.pop());
+        }
+
+        sb.reverse();
+
+        System.out.println(sb.toString());
+
+        while (sb.length() > 1 && sb.charAt(0) == '0') {
+
+            sb.deleteCharAt(0);
+        }
+
+        return sb.toString();
+    }
+}
