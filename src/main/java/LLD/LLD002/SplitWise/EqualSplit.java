85244615061159b51294b4667a94f282443670f5
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2024-09-10 02:13:45 +0530

    dsa musings 2024

---
+++ src/main/java/LLD/LLD002/SplitWise/EqualSplit.java
@@ -0,0 +1,18 @@
+package LLD.LLD002.SplitWise;
+
+
+
+public class EqualSplit extends Split {
+    public EqualSplit(User user) {
+        super(user);
+    }
+
+    @Override
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
