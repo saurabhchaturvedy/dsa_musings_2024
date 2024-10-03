9feb1f0e4c2365e1140d353084ed93500ffbb6d9
Saurabh Chaturvedi <saurabhchaturvedy@gmail.com>
2024-10-03 20:20:27 +0530

    dsa musings 2024

--- src/main/java/Atlass/ReverseNumber.java
+++ src/main/java/Atlass/ReverseNumber.java
@@ -1,2 +1,26 @@
-package Atlass;public class ReverseNumber {
+package Atlass;
+
+public class ReverseNumber {
+
+
+    public static int reverse(int x) {
+
+        int rem = 0;
+        int rev = 0;
+
+        while (x > 0) {
+
+            rem = x % 10;
+            rev = rev * 10 + rem;
+            x = x / 10;
+        }
+
+        return rev;
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println(" reversed number is = " + reverse(123));
+    }
 }
