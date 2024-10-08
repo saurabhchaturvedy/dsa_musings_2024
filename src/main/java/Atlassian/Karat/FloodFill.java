bec0849efa6b217a66a2fc3d28325af96b6c18de
Chaturvedi, Saurabh <saurabhchaturvedy@gmail.com>
2024-10-08 20:09:21 +0530

    dsa musings 2024

--- src/main/java/Atlassian/Karat/FloodFill.java
+++ src/main/java/Atlassian/Karat/FloodFill.java
@@ -1,21 +1,20 @@
 package Atlassian.Karat;
 
-public class FloodFill {
+import java.util.Arrays;
 
+public class FloodFill {
 
 
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
 
-        if (color == image[sr][sc])
-            return image;
+        if (color == image[sr][sc]) return image;
         dfs(image, sr, sc, color, image[sr][sc]);
         return image;
     }
 
     public void dfs(int[][] image, int sr, int sc, int newColor, int oldColor) {
 
-        if (sr >= image.length || sc >= image[0].length || sr < 0 || sc < 0 || image[sr][sc] != oldColor)
-            return;
+        if (sr >= image.length || sc >= image[0].length || sr < 0 || sc < 0 || image[sr][sc] != oldColor) return;
 
         image[sr][sc] = newColor;
 
@@ -28,6 +27,24 @@ public class FloodFill {
 
 
     public static void main(String[] args) {
+
+
+        int[][] image = {
+
+                {1, 1, 1}, {1, 1, 0}, {1, 0, 1}};
+
+
+        FloodFill floodFill = new FloodFill();
+
+        image = floodFill.floodFill(image, 1, 1, 2);
+
+
+        Arrays.stream(image).forEach(row -> {
+            Arrays.stream(row).forEach(x -> {
+
+                System.out.print(row + " ");
+            });
+        });
 
     }
 
