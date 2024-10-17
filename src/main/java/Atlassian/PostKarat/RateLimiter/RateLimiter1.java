82ae97d56e10989aaacf735bc6494703659c8c51
Chaturvedi, Saurabh <saurabhchaturvedy@gmail.com>
2024-10-17 19:29:28 +0530

    dsa musings 2024

--- src/main/java/Atlassian/PostKarat/RateLimiter/RateLimiter1.java
+++ src/main/java/Atlassian/PostKarat/RateLimiter/RateLimiter1.java
@@ -29,13 +29,13 @@ public class RateLimiter1 {
         Queue<Long> timestamps = userTimeStamps.getOrDefault(customerId, new LinkedList<>());
 
 
-        System.out.println(" Current Time : "+currentTime);
-        System.out.println(" Queue Timestamp Peek Value : "+timestamps.peek());
-        System.out.println("Time difference = "+(currentTime - (timestamps.peek()==null ? 0 : timestamps.peek())));
-        System.out.println(" Queue size : "+timestamps.size());
-        System.out.println("Window Size For Rate Limit : "+windowSizeForRateLimitInMs);
+        System.out.println(" Current Time : " + currentTime);
+        System.out.println(" Queue Timestamp Peek Value : " + timestamps.peek());
+        System.out.println("Time difference = " + (currentTime - (timestamps.peek() == null ? 0 : timestamps.peek())));
+        System.out.println(" Queue size : " + timestamps.size());
+        System.out.println("Window Size For Rate Limit : " + windowSizeForRateLimitInMs);
 
-        long timeStampDifference = currentTime - (timestamps.peek()==null ? 0 : timestamps.peek());
+        long timeStampDifference = currentTime - (timestamps.peek() == null ? 0 : timestamps.peek());
         while (!timestamps.isEmpty() && timeStampDifference > windowSizeForRateLimitInMs) {
             System.out.println(" Polling Queue :");
             timestamps.poll();
@@ -60,37 +60,19 @@ public class RateLimiter1 {
 
         RateLimiter1 rateLimiter1 = new RateLimiter1(3, 10);
 
+        long currTime = System.currentTimeMillis();
+        for (int i = 0; i < 20; i++) {
 
-//        for (int i = 0; i < 10; i++) {
-//
-//            boolean isAllowed = rateLimiter1.rateLimit(1);
-//            System.out.println(" Request " + (i + 1) + " Is Allowed ? " + isAllowed);
-//            try {
-//                Thread.sleep(1000);
-//            } catch (InterruptedException e) {
-//
-//                Thread.currentThread().interrupt();
-//            }
-//        }
+            boolean isAllowed = rateLimiter1.rateLimit(1);
+            System.out.println(" Request " + (i + 1) + " Is Allowed ? " + isAllowed);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
 
-
-
-        RateLimiter1 rateLimiter2 = new RateLimiter1(3, 10); // 3 requests per 10 seconds
-        int customerId = 1;
-
-        // Simulate requests
-        for (int i = 0; i < 10; i++) { // Attempt to make 5 requests
-            boolean allowed = rateLimiter2.rateLimit(customerId);
-            System.out.println("Request " + (i + 1) + " allowed: " + allowed);
-            Thread.sleep(2000); // Wait 2 seconds between requests to simulate time passing
+                Thread.currentThread().interrupt();
+            }
         }
+        System.out.println(" Total time elapsed = " + (System.currentTimeMillis() - currTime) / 1000 + " seconds ");
 
-
-        // Simulate additional requests after the time window has elapsed
-        Thread.sleep(8000); // Wait enough time to reset the rate limit
-        for (int i = 0; i < 2; i++) { // Attempt to make 2 more requests
-            boolean allowed = rateLimiter2.rateLimit(customerId);
-            System.out.println("Request " + (i + 6) + " allowed: " + allowed);
-        }
     }
 }
