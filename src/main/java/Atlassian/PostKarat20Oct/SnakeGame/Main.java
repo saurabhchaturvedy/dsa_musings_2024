06257ce0b458f695c53f5a597ec066151d3627f7
Chaturvedi, Saurabh <saurabhchaturvedy@gmail.com>
2024-10-22 01:35:41 +0530

    dsa musings 2024

--- src/main/java/Atlassian/PostKarat20Oct/SnakeGame/Main.java
+++ src/main/java/Atlassian/PostKarat20Oct/SnakeGame/Main.java
@@ -8,13 +8,15 @@ public class Main {
 
         SnakeGameImpl snakeGame = new SnakeGameImpl(5, 5);
 
-        System.out.println(" Snake Directions : " + snakeGame.getSnakeDirections());
+       // System.out.println(" Snake Directions : " + snakeGame.getSnakeDirections());
+
+        snakeGame.publishBoard();
 
         System.out.println(" ******** Start Game *******");
+       // moveSnake(snakeGame, Direction.UP, 2);
         moveSnake(snakeGame, Direction.RIGHT, 2);
         moveSnake(snakeGame, Direction.DOWN, 3);
-        moveSnake(snakeGame, Direction.LEFT, 1);
-        moveSnake(snakeGame, Direction.RIGHT, 1);
+        moveSnake(snakeGame, Direction.LEFT, 3);
 
 
         if (snakeGame.isGameOver()) {
@@ -23,7 +25,7 @@ public class Main {
             System.out.println(" ******** Game Over !! *******");
         } else {
 
-            System.out.println(" Snake Directions : " + snakeGame.getSnakeDirections());
+            snakeGame.publishBoard();
         }
     }
 
@@ -34,7 +36,9 @@ public class Main {
 
 
             snakeGame.moveSnake(direction);
+            snakeGame.publishBoard();
             System.out.println(" Direction = " + direction.name() + " : " + snakeGame.getSnakeDirections());
+            System.out.println(" Total steps = "+snakeGame.steps);
             System.out.println(" Snake Size = " + snakeGame.getSnakeDirections().size());
         }
     }
--- src/main/java/Atlassian/PostKarat20Oct/SnakeGame/SnakeGameImpl.java
+++ src/main/java/Atlassian/PostKarat20Oct/SnakeGame/SnakeGameImpl.java
@@ -2,6 +2,7 @@ package Atlassian.PostKarat20Oct.SnakeGame;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 
 public class SnakeGameImpl implements SnakeGame {
 
@@ -11,6 +12,9 @@ public class SnakeGameImpl implements SnakeGame {
     int boardWidth;
     int boardHeight;
     boolean isGameOver;
+    char[][] board;
+    Cell foodPosition;
+    Random random;
 
     SnakeGameImpl(int boardWidth, int boardHeight) {
         this.boardWidth = boardWidth;
@@ -21,6 +25,52 @@ public class SnakeGameImpl implements SnakeGame {
         this.snake.add(new Cell(0, 0));
         this.snake.add(new Cell(0, 1));
         this.snake.add(new Cell(0, 2));
+        this.board = new char[boardHeight][boardHeight];
+        this.random = new Random();
+        placeFood(3, 2);
+        initializeBoard();
+
+    }
+
+    public void initializeBoard() {
+
+        for (int i = 0; i < boardWidth; i++) {
+            for (int j = 0; j < boardHeight; j++) {
+
+                Cell cell = new Cell(i, j);
+                if (!snake.contains(cell)) {
+                    if (foodPosition.x == cell.x && foodPosition.y == cell.y) {
+                        board[i][j] = 'F';
+                    } else {
+                        board[i][j] = '-';
+                    }
+                } else {
+
+                    if (snake.getLast().x == cell.x && snake.getLast().y == cell.y) {
+                        board[i][j] = '$';
+                    } else {
+
+                        board[i][j] = 'X';
+                    }
+
+                }
+
+            }
+
+        }
+    }
+
+
+    public void publishBoard() {
+
+        for (int i = 0; i < boardWidth; i++) {
+            for (int j = 0; j < boardHeight; j++) {
+
+                System.out.print(board[i][j]);
+
+            }
+            System.out.println();
+        }
     }
 
 
@@ -43,13 +93,17 @@ public class SnakeGameImpl implements SnakeGame {
         snake.add(newHead);
         steps++;
 
-        if (steps % 5 == 0) {
+        if (steps % 5 == 0 || snake.contains(foodPosition)) {
+
+            if (snake.contains(foodPosition)) {
+                placeFood(0, 4);
+            }
 
         } else {
 
             this.snake.removeFirst();
         }
-
+        this.initializeBoard();
     }
 
     private Cell getNextCell(Direction direction) {
@@ -86,5 +140,23 @@ public class SnakeGameImpl implements SnakeGame {
 
     public List<Cell> getSnakeDirections() {
         return snake;
+    }
+
+
+    public void placeFood(int xx, int yy) {
+
+        while (true) {
+
+            int x = random.nextInt(this.boardWidth);
+            int y = random.nextInt(this.boardHeight);
+
+            Cell newFood = new Cell(xx, yy);
+
+            if (!snake.contains(newFood)) {
+
+                foodPosition = newFood;
+                break;
+            }
+        }
     }
 }
